bfd4a2e9a7dd8f39fcc71a85aa5a117922929964
Ian G <ian.glazer@gmail.com>
2013-12-04 20:35:00 -0500

    added sample json

--- redis_test.js
+++ redis_test.js
@@ -4,7 +4,9 @@
         console.log("Error " + err);
     });
     
-	var jsonBlob = "foo";
+	var jsonBlob = "{" +
+			"imageID hash of file:{"+
+			"filename":"myfile.png}}";
 	client.set("imageID1", jsonBlob, redis.print);
 	client.get("imageID1", function (err, res) {
 		console.log(res);
